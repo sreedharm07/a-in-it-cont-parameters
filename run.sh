4d8661644dd51418e15e89afbef5fa2a8d30834c
sreedhar <sreedhar70755@gmail.com>
2023-10-17 07:53:24 +0530

    tf

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 for parameter in catalogue.pod.mongo_url,MONGO_URL catalogue.prod.DOCUMENTDB,DOCUMENTDB; do
   ssm_parameter=$(echo $parameter | awk -F, '{print $1}')
   app_parameter=$(echo $parameter | awk -F, '{print $2}')
-  aws ssm get-parameter --name catalogue.pod.mongo_url --with-decryption | jq .Parameter.Value | sed -e 's/"//g'
+  aws ssm get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g'
 done
