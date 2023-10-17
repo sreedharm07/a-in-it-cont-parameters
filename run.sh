f43087744448019fb268c1b05022b01933469ea6
sreedhar <sreedhar70755@gmail.com>
2023-10-17 19:12:18 +0530

    tf

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ for parameter in $PARAMETERS; do
   app_parameter=$(echo $parameter | awk -F, '{print $2}')
   echo export ${app_parameter}=\"$(aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/common/parameters
 done
+
