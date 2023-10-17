42275627ff186302de2575652618e07a9667be0f
sreedhar <sreedhar70755@gmail.com>
2023-10-17 11:34:04 +0530

    tf

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 for parameter in catalogue.pod.mongo_url,MONGO_URL catalogue.prod.DOCUMENTDB,DOCUMENTDB; do
   ssm_parameter=$(echo $parameter | awk -F, '{print $1}')
   app_parameter=$(echo $parameter | awk -F, '{print $2}')
-echo export ${ssm_parameter}=$(aws ssm get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g') >>/common/parameters
+  echo export ${ssm_parameter}=$(aws ssm get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g') >>/common/parameters
 done
